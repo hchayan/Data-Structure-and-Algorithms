95f0a7155808414142a52fdbfcc292d2f25b2715
ChayanRin <k852012@naver.com>
2020-03-31 14:54:44 +0900

    Update graph basic concepts

---
+++ Quizes/backjoon/back_2579.py
@@ -0,0 +1,15 @@
+
+def stair(i, lis):
+    global stairs
+    if i == stairs:
+        return
+
+
+
+
+stairs = int(input())
+lis = []
+for i in range(stairs):
+    lis.append(int(input()))
+
+print(stairs(0, lis))
